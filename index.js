f162b24f1feadb92ab30dad7fcb58f72a3b4add5
Rossy2906 <rociopilar.j29@gmail.com>
2023-05-22 15:29:34 +0200

    ...

--- index.js
+++ index.js
@@ -14,6 +14,31 @@ document.querySelector('#search').onclick=() =>{
 
 var swiper = new Swiper(".product-row", {
     spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });  
+  var swiper = new Swiper(".blogs-row", {
+    spaceBetween: 30,
+    loop:true,
     centeredSlides:true,
     autoplay:{
         delay:9500,
